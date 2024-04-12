6eb0fd46c7aefc4637593d8e3e4aee715d5da2fe
Samuel Coelho <151157545+Samuelnwg@users.noreply.github.com>
2024-04-12 11:56:03 -0300

    Add files via upload

---
+++ VisualizadorImagens/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace VisualizadorImagens
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picFoto = new System.Windows.Forms.PictureBox();
+            this.btnFoto1 = new System.Windows.Forms.Button();
+            this.btnFoto2 = new System.Windows.Forms.Button();
+            this.btnFoto3 = new System.Windows.Forms.Button();
+            this.btnFoto4 = new System.Windows.Forms.Button();
+            this.btnFoto5 = new System.Windows.Forms.Button();
+            this.pnlControle = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rdbFormulario3 = new System.Windows.Forms.RadioButton();
+            this.rdbFormulario2 = new System.Windows.Forms.RadioButton();
+            this.rdbFormulario1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rdbCorBotao1 = new System.Windows.Forms.RadioButton();
+            this.rdbCorbotao2 = new System.Windows.Forms.RadioButton();
+            this.rdbCorBotao3 = new System.Windows.Forms.RadioButton();
+            this.grpFormulario = new System.Windows.Forms.GroupBox();
+            this.grpBotoes = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picFoto)).BeginInit();
+            this.pnlControle.SuspendLayout();
+            this.grpFormulario.SuspendLayout();
+            this.grpBotoes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picFoto
+            // 
+            this.picFoto.Location = new System.Drawing.Point(12, 12);
+            this.picFoto.Name = "picFoto";
+            this.picFoto.Size = new System.Drawing.Size(969, 583);
+            this.picFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picFoto.TabIndex = 0;
+            this.picFoto.TabStop = false;
+            // 
+            // btnFoto1
+            // 
+            this.btnFoto1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFoto1.Location = new System.Drawing.Point(28, 692);
+            this.btnFoto1.Name = "btnFoto1";
+            this.btnFoto1.Size = new System.Drawing.Size(225, 59);
+            this.btnFoto1.TabIndex = 1;
+            this.btnFoto1.Text = "FOTO 1";
+            this.btnFoto1.UseVisualStyleBackColor = true;
+            this.btnFoto1.Click += new System.EventHandler(this.btnFoto1_Click);
+            // 
+            // btnFoto2
+            // 
+            this.btnFoto2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFoto2.Location = new System.Drawing.Point(269, 692);
+            this.btnFoto2.Name = "btnFoto2";
+            this.btnFoto2.Size = new System.Drawing.Size(219, 59);
+            this.btnFoto2.TabIndex = 1;
+            this.btnFoto2.Text = "FOTO 2";
+            this.btnFoto2.UseVisualStyleBackColor = true;
+            this.btnFoto2.Click += new System.EventHandler(this.btnFoto2_Click);
+            // 
+            // btnFoto3
+            // 
+            this.btnFoto3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFoto3.Location = new System.Drawing.Point(504, 692);
+            this.btnFoto3.Name = "btnFoto3";
+            this.btnFoto3.Size = new System.Drawing.Size(217, 59);
+            this.btnFoto3.TabIndex = 1;
+            this.btnFoto3.Text = "FOTO 3";
+            this.btnFoto3.UseVisualStyleBackColor = true;
+            this.btnFoto3.Click += new System.EventHandler(this.btnFoto3_Click);
+            // 
+            // btnFoto4
+            // 
+            this.btnFoto4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFoto4.Location = new System.Drawing.Point(737, 692);
+            this.btnFoto4.Name = "btnFoto4";
+            this.btnFoto4.Size = new System.Drawing.Size(244, 59);
+            this.btnFoto4.TabIndex = 1;
+            this.btnFoto4.Text = "FOTO 4";
+            this.btnFoto4.UseVisualStyleBackColor = true;
+            this.btnFoto4.Click += new System.EventHandler(this.btnFoto4_Click);
+            // 
+            // btnFoto5
+            // 
+            this.btnFoto5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFoto5.Location = new System.Drawing.Point(997, 692);
+            this.btnFoto5.Name = "btnFoto5";
+            this.btnFoto5.Size = new System.Drawing.Size(250, 59);
+            this.btnFoto5.TabIndex = 1;
+            this.btnFoto5.Text = "FOTO 5";
+            this.btnFoto5.UseVisualStyleBackColor = true;
+            this.btnFoto5.Click += new System.EventHandler(this.btnFoto5_Click);
+            // 
+            // pnlControle
+            // 
+            this.pnlControle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pnlControle.Controls.Add(this.grpBotoes);
+            this.pnlControle.Controls.Add(this.grpFormulario);
+            this.pnlControle.Controls.Add(this.label2);
+            this.pnlControle.Controls.Add(this.label1);
+            this.pnlControle.Location = new System.Drawing.Point(1005, 15);
+            this.pnlControle.Name = "pnlControle";
+            this.pnlControle.Size = new System.Drawing.Size(241, 579);
+            this.pnlControle.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cor do Formulario";
+            // 
+            // rdbFormulario3
+            // 
+            this.rdbFormulario3.AutoSize = true;
+            this.rdbFormulario3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbFormulario3.Location = new System.Drawing.Point(16, 98);
+            this.rdbFormulario3.Name = "rdbFormulario3";
+            this.rdbFormulario3.Size = new System.Drawing.Size(93, 24);
+            this.rdbFormulario3.TabIndex = 2;
+            this.rdbFormulario3.TabStop = true;
+            this.rdbFormulario3.Text = "Amarelo";
+            this.rdbFormulario3.UseVisualStyleBackColor = true;
+            // 
+            // rdbFormulario2
+            // 
+            this.rdbFormulario2.AutoSize = true;
+            this.rdbFormulario2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbFormulario2.Location = new System.Drawing.Point(16, 59);
+            this.rdbFormulario2.Name = "rdbFormulario2";
+            this.rdbFormulario2.Size = new System.Drawing.Size(75, 24);
+            this.rdbFormulario2.TabIndex = 1;
+            this.rdbFormulario2.TabStop = true;
+            this.rdbFormulario2.Text = "Verde";
+            this.rdbFormulario2.UseVisualStyleBackColor = true;
+            // 
+            // rdbFormulario1
+            // 
+            this.rdbFormulario1.AutoSize = true;
+            this.rdbFormulario1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbFormulario1.Location = new System.Drawing.Point(16, 19);
+            this.rdbFormulario1.Name = "rdbFormulario1";
+            this.rdbFormulario1.Size = new System.Drawing.Size(62, 24);
+            this.rdbFormulario1.TabIndex = 0;
+            this.rdbFormulario1.TabStop = true;
+            this.rdbFormulario1.Text = "Azul";
+            this.rdbFormulario1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cor dos Botões";
+            // 
+            // rdbCorBotao1
+            // 
+            this.rdbCorBotao1.AccessibleDescription = "";
+            this.rdbCorBotao1.AutoSize = true;
+            this.rdbCorBotao1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbCorBotao1.Location = new System.Drawing.Point(17, 16);
+            this.rdbCorBotao1.Name = "rdbCorBotao1";
+            this.rdbCorBotao1.Size = new System.Drawing.Size(133, 24);
+            this.rdbCorBotao1.TabIndex = 5;
+            this.rdbCorBotao1.TabStop = true;
+            this.rdbCorBotao1.Text = "Cinza Escuro";
+            this.rdbCorBotao1.UseVisualStyleBackColor = true;
+            this.rdbCorBotao1.CheckedChanged += new System.EventHandler(this.rdbCorBotao1_CheckedChanged);
+            // 
+            // rdbCorbotao2
+            // 
+            this.rdbCorbotao2.AutoSize = true;
+            this.rdbCorbotao2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbCorbotao2.Location = new System.Drawing.Point(17, 52);
+            this.rdbCorbotao2.Name = "rdbCorbotao2";
+            this.rdbCorbotao2.Size = new System.Drawing.Size(87, 24);
+            this.rdbCorbotao2.TabIndex = 6;
+            this.rdbCorbotao2.TabStop = true;
+            this.rdbCorbotao2.Text = "Laranja";
+            this.rdbCorbotao2.UseVisualStyleBackColor = true;
+            this.rdbCorbotao2.CheckedChanged += new System.EventHandler(this.rdbCorbotao2_CheckedChanged);
+            // 
+            // rdbCorBotao3
+            // 
+            this.rdbCorBotao3.AutoSize = true;
+            this.rdbCorBotao3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbCorBotao3.Location = new System.Drawing.Point(17, 89);
+            this.rdbCorBotao3.Name = "rdbCorBotao3";
+            this.rdbCorBotao3.Size = new System.Drawing.Size(83, 24);
+            this.rdbCorBotao3.TabIndex = 7;
+            this.rdbCorBotao3.TabStop = true;
+            this.rdbCorBotao3.Text = "Violeta";
+            this.rdbCorBotao3.UseVisualStyleBackColor = true;
+            this.rdbCorBotao3.CheckedChanged += new System.EventHandler(this.rdbCorBotao3_CheckedChanged);
+            // 
+            // grpFormulario
+            // 
+            this.grpFormulario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.grpFormulario.Controls.Add(this.rdbFormulario3);
+            this.grpFormulario.Controls.Add(this.rdbFormulario2);
+            this.grpFormulario.Controls.Add(this.rdbFormulario1);
+            this.grpFormulario.Location = new System.Drawing.Point(16, 43);
+            this.grpFormulario.Name = "grpFormulario";
+            this.grpFormulario.Size = new System.Drawing.Size(209, 128);
+            this.grpFormulario.TabIndex = 8;
+            this.grpFormulario.TabStop = false;
+            this.grpFormulario.Text = "Formulário";
+            // 
+            // grpBotoes
+            // 
+            this.grpBotoes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.grpBotoes.Controls.Add(this.rdbCorBotao3);
+            this.grpBotoes.Controls.Add(this.rdbCorbotao2);
+            this.grpBotoes.Controls.Add(this.rdbCorBotao1);
+            this.grpBotoes.Location = new System.Drawing.Point(16, 248);
+            this.grpBotoes.Name = "grpBotoes";
+            this.grpBotoes.Size = new System.Drawing.Size(209, 134);
+            this.grpBotoes.TabIndex = 9;
+            this.grpBotoes.TabStop = false;
+            this.grpBotoes.Text = "Botões";
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 778);
+            this.Controls.Add(this.pnlControle);
+            this.Controls.Add(this.btnFoto5);
+            this.Controls.Add(this.btnFoto4);
+            this.Controls.Add(this.btnFoto3);
+            this.Controls.Add(this.btnFoto2);
+            this.Controls.Add(this.btnFoto1);
+            this.Controls.Add(this.picFoto);
+            this.Name = "FrmPrincipal";
+            this.Text = "Visualizador Simples de Imagens";
+            ((System.ComponentModel.ISupportInitialize)(this.picFoto)).EndInit();
+            this.pnlControle.ResumeLayout(false);
+            this.pnlControle.PerformLayout();
+            this.grpFormulario.ResumeLayout(false);
+            this.grpFormulario.PerformLayout();
+            this.grpBotoes.ResumeLayout(false);
+            this.grpBotoes.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picFoto;
+        private System.Windows.Forms.Button btnFoto1;
+        private System.Windows.Forms.Button btnFoto2;
+        private System.Windows.Forms.Button btnFoto3;
+        private System.Windows.Forms.Button btnFoto4;
+        private System.Windows.Forms.Button btnFoto5;
+        private System.Windows.Forms.Panel pnlControle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rdbFormulario3;
+        private System.Windows.Forms.RadioButton rdbFormulario2;
+        private System.Windows.Forms.RadioButton rdbFormulario1;
+        private System.Windows.Forms.RadioButton rdbCorBotao3;
+        private System.Windows.Forms.RadioButton rdbCorbotao2;
+        private System.Windows.Forms.RadioButton rdbCorBotao1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox grpBotoes;
+        private System.Windows.Forms.GroupBox grpFormulario;
+    }
+}
+
---
+++ VisualizadorImagens/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisualizadorImagens
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFoto1_Click(object sender, EventArgs e)
+        {
+            picFoto.Image = Properties.Resources.foto01;
+        }
+
+        private void btnFoto2_Click(object sender, EventArgs e)
+        {
+            picFoto.Image = Properties.Resources.foto02;
+        }
+
+        private void btnFoto3_Click(object sender, EventArgs e)
+        {
+            picFoto.Image = Properties.Resources.foto03;
+        }
+
+        private void btnFoto4_Click(object sender, EventArgs e)
+        {
+            picFoto.Image = Properties.Resources.foto04;
+        }
+
+        private void btnFoto5_Click(object sender, EventArgs e)
+        {
+            picFoto.Image = Properties.Resources.foto06;
+        }
+
+        private void rdbCorBotao1_CheckedChanged(object sender, EventArgs e)
+        {
+            btnFoto1.BackColor = Color.Gray;
+            btnFoto2.BackColor = Color.Gray;
+            btnFoto3.BackColor = Color.Gray;
+            btnFoto4.BackColor = Color.Gray;
+            btnFoto5.BackColor = Color.Gray;
+
+        }
+
+        private void rdbCorbotao2_CheckedChanged(object sender, EventArgs e)
+        {
+            btnFoto1.BackColor = Color.Orange;
+            btnFoto2.BackColor = Color.Orange;
+            btnFoto3.BackColor = Color.Orange;
+            btnFoto4.BackColor = Color.Orange;
+            btnFoto5.BackColor = Color.Orange;
+        }
+
+        private void rdbCorBotao3_CheckedChanged(object sender, EventArgs e)
+        {
+            btnFoto1.BackColor = Color.Violet;
+            btnFoto2.BackColor = Color.Violet;
+            btnFoto3.BackColor = Color.Violet;
+            btnFoto4.BackColor = Color.Violet;
+            btnFoto5.BackColor = Color.Violet;
+        }
+    }
+}
